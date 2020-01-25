618493823218ec5fb7c29f6c4ba3408bb4fc3758
Milton Llera <m.lleramontero@bristol.ac.uk>
2020-01-25 16:43:45 +0000

    Update model loading function.

--- src/models/utils.py
+++ src/models/utils.py
@@ -3,15 +3,11 @@ import torch
 import yaml
 
 
-def load_meta(path):
-    with open(path, mode='r') as f:
-        meta = yaml.load(f, yaml.FullLoader)
-    return meta
-
-
 def load_model(model_folder, inner_loader, device='cuda'):
     for file in os.listdir(model_folder):
         if file.endswith((".p", ".pth")):
-            file = os.path.join(model_folder, file)
-            meta = os.path.join(model_folder, 'meta.yaml')
-            return inner_loader(meta, file, device)
+            with open(os.path.join(model_folder, 'meta.yaml'), mode='r') as f:
+                meta = yaml.load(f, yaml.FullLoader)
+            with open(os.path.join(model_folder, file), mode='rb') as f:
+                state_dict = torch.load(f)
+            return inner_loader(meta['model-params'], state, device)
