67be6772f50539913a08d9c366ec1f5ff6a0d5a3
Milton Llera <m.lleramontero@bristol.ac.uk>
2020-01-25 16:42:30 +0000

    Add transposed convolution.

--- src/models/convolution.py
+++ src/models/convolution.py
@@ -1,8 +1,10 @@
 import math
+import numpy as np
 from collections import namedtuple
 
 import torch
 import torch.nn as nn
+from .mlp import get_nonlinearity
 
 
 KernelParams = namedtuple('KernelParams',
@@ -27,115 +29,120 @@ def create_pool(kernel_size, stride, padding, mode):
         raise ValueError('Unrecognised pooling mode {}'.format(mode))
 
 
-def _get_out_features(*args):
-    # expand params
-    params = [p if isinstance(p, tuple) else (p,p) for p in args]
+def _pair(s):
+    if not isinstance(s, tuple):
+        return s, s
+    return s
 
-    hval, wval = zip(*params)
 
-    # new output shape
+def conv2d_out_shape(in_shape, out_channels, kernel_shape, stride, padding):
+    in_shape = in_shape[1:]
+    kernel_shape = _pair(kernel_shape)
+    stride = _pair(stride)
+    padding = _pair(padding)
+
+    hval, wval = zip(in_shape, kernel_shape, stride, padding)
+    
     hout = math.floor((hval[0] -  hval[1] + 2 * hval[3]) / hval[2]) + 1
-    hout = math.floor((hout - hval[4] + 2 * hval[6]) / hval[5]) + 1
-
     wout = math.floor((wval[0] -  wval[1] + 2 * wval[3]) / wval[2]) + 1
-    wout = math.floor((wout - wval[4] + 2 * wval[6]) / wval[5]) + 1
 
-    return hout, wout
+    return out_channels, hout, wout
 
 
-def get_conv2D_out_features(input_shape, kernels, pools):
-    out_chann, hin, win = input_shape
-    out_shape = hin, win
+def maxpool2d_out_shape(in_shape, pool_shape, stride, padding):
+    in_channels, hout, wout = in_shape
+    pool_shape = _pair(pool_shape)
+    stride = _pair(stride)
+    padding = _pair(padding)
+
+    hval, wval = zip(pool_shape, stride, padding)
+    
+    hout = math.floor((hout - hval[0] + 2 * hval[2]) / hval[1]) + 1
+    wout = math.floor((wout - wval[0] + 2 * wval[2]) / wval[1]) + 1
+
+    return in_channels, hout, wout
+
+
+def get_conv_layer_out_shape(input_shape, kernels, pools):
+    out_shape = input_shape
 
     for k, p in zip(kernels, pools):
-        kout, ksize, kstride, kpad = k
-        psize, pstride, ppad, _ = p
+        out_shape = conv2d_out_shape(out_shape, *k)
+        out_shape = maxpool2d_out_shape(out_shape, *p[:-1])
 
-        out_chann = kout
-        out_shape = _get_out_features(
-            out_shape,ksize, kstride,
-            kpad, psize, pstride, ppad
-        )
-
-    return out_chann, out_shape[0], out_shape[1]
+    return out_shape
 
 
-def unflatten(batch, shape):
-    channels, w, h = shape
-    return batch.view(-1, channels, w, h)
+class CNN(nn.Module):
+    def __init__(self, input_shape, kernels, pools, 
+                 non_linearity='relu', dropout=0.0):
+        super().__init__()
+        if pools is None:
+            pools = len(kernels) * [None]
+
+        elif len(pools) != len(kernels):
+            raise ValueError(
+                'Number of pooling  and convolution layers do not match')
+
+        in_chann, h, w = input_shape
+
+        conv_layers = []
+        for conv_shape, pooling_params in zip(kernels, pools):
+            conv_layers.extend([
+                nn.Conv2d(in_chann, *conv_shape),
+                get_nonlinearity(non_linearity)(),
+            ])
+            if pooling_params is not None:
+                conv_layers.append(create_pool(*pooling_params))
+
+            in_chann = conv_shape[0]
+
+        self.conv_layers = nn.Sequential(*conv_layers)
+        self.input_shape = input_shape
+        self.out_shape = np.prod(get_conv_layer_out_shape(
+            input_shape, kernels, pools))
+
+    def forward(self, inputs):
+        inputs = inputs.view(-1, *self.input_shape)
+        outputs = self.conv_layers(inputs)
+        return torch.flatten(outputs, start_dim=1)
 
 
-def flatten(batch, size):
-    return batch.view(-1, size)
+def _bilinear_usample(in_shape, kernel, pool):
+    upsample_shape = conv2d_out_shape(in_shape, *kernel)
+    usample = nn.UpsamplingBilinear2d(size=upsample_shape[1:])
+    in_shape = maxpool2d_out_shape(
+        upsample_shape, *pool[:-1])
+    return usample, in_shape
 
 
-def create_conv_layers(input_shape, kernels, pools, dropout=0.0, non_linearity='ReLU'):
-    in_chann, h, w = input_shape
+class tCNN(nn.Module):
+    def __init__(self, input_shape, kernels, pools, 
+                 non_linearity='relu', dropout=0.0):
+        super().__init__()
+        in_chann, h, w = input_shape
+        if pools is None:
+            pools = len(kernels) * [None]
 
-    conv_layers = []
-    for conv_shape, pool_shape in zip(kernels, pools):
-        conv_layers.extend([
-            nn.Conv2d(in_chann, *conv_shape),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            create_pool(*pool_shape)
-        ])
+        conv_layers, layer_input = [], input_shape
+        for kernel, pool in zip(kernels, pools):
+            out_channel = kernel[0]
 
-        in_chann = conv_shape.outchannels
+            conv = nn.ConvTranspose2d(out_channel, in_chann, *kernel[1:])
+            conv_layers.extend([conv, get_nonlinearity(non_linearity)()])
 
-    return nn.Sequential(*conv_layers)
+            if pool is not None:
+                upsample, layer_input = _bilinear_usample(layer_input,
+                                                          kernel, pool)
+                conv_layers.append(upsample)
+                                                        
+            in_chann = out_channel
 
+        self.tconv_layers = nn.Sequential(*reversed(conv_layers))
+        self.input_shape = get_conv_layer_out_shape(input_shape, kernels, pools)
+        self.out_shape = input_shape
 
-def init_cnn_layers(layer_type):
-    if layer_type == '2-layer':
-        kernels = [
-            KernelParams(
-                outchannels=3, shape=(2, 2), stride=(1, 1), padding=(1, 1)),
-        ]
-
-        pools = [
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-        ]
-
-    elif layer_type == '4-layer':
-        kernels = [
-            KernelParams(
-                outchannels=16, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-            KernelParams(
-                outchannels=32, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-        ]
-
-        pools = [
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max')
-        ]
-
-    elif layer_type == '6-layer':
-        kernels = [
-            KernelParams(outchannels=32, shape=(3, 3), stride=(1, 1), padding=(1, 1)),
-            KernelParams(outchannels=64, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-            KernelParams(outchannels=128, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-        ]
-
-        pools = [
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max')
-        ]
-
-    elif layer_type == '8-layer':
-        kernels = [
-            KernelParams(outchannels=32, shape=(3, 3), stride=(1, 1), padding=(1, 1)),
-            KernelParams(outchannels=64, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-            KernelParams(outchannels=128, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-            KernelParams(outchannels=256, shape=(5, 5), stride=(1, 1), padding=(1, 1)),
-        ]
-
-        pools = [
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-            PoolingParams(shape=(2, 2), stride=(1, 1), padding=(0, 0), mode='max'),
-    ]
-
-    return kernels, pools
+    def forward(self, inputs):
+        inputs = inputs.view(-1, *self.input_shape)
+        outputs = self.tconv_layers(inputs)
+        return torch.flatten(outputs, start_dim=1)
