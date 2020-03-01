f5f5f87d70ecbb2455dd6a57f6d600c96b8c757b
Milton Llera <m.lleramontero@bristol.ac.uk>
2020-03-01 13:53:49 +0000

    Refactor model loading.

--- src/models/utils.py
+++ src/models/utils.py
@@ -9,9 +9,15 @@ def load_meta(path):
     return meta
 
 
-def load_model(model_folder, inner_loader, device='cuda'):
+def from_file(model_folder, inner_loader, device='cuda'):
     for file in os.listdir(model_folder):
         if file.endswith((".p", ".pth")):
             file = os.path.join(model_folder, file)
             meta = os.path.join(model_folder, 'meta.yaml')
-            return inner_loader(meta, file, device)
+            
+            meta = load_meta(meta)
+
+            with open(file, mode='rb') as f:
+                state_dict = torch.load(f)
+
+            return inner_loader(meta['model-params'], state_dict, device), meta
